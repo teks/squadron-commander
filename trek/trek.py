c4ba5455281c4bebac6fa52d1eda33aea05584f3
teks <>
2024-04-16 10:18:59 -0400

    use sensible cartesian coordinate system

--- trek/trek.py
+++ trek/trek.py
@@ -1,32 +1,29 @@
 """Probably the whole dang game engine is gonna go in here.
 
-TYPING: 2D points are just 2-tuples. Most other things (spaceborne objects,
-cartographic entities like 'quadrants') are instances of classes.
 """
 
 import typing
 
-MAX_HORIZ = 64
-MAX_VERT = 64
+MAX_X = 64
+MAX_Y = 64
 
 
 class Point(typing.NamedTuple):
-    """Bounds are from (1, 1) to (MAX_HORIZ, MAX_VERT), inclusive."""
-    horiz: int
-    vert: int
+    """Bounds are from (1, 1) to (MAX_X, MAX_Y), inclusive."""
+    x: int
+    y: int
 
     def distance(self, other):
         """Computes the distance between a and b."""
         # recall the distance formula doesn't care which way is up:
         ax, ay = self
         bx, by = other
-        d = ((ax - bx) ** 2 + (ay - by) ** 2) ** 0.5
+        d = ((ax - bx)**2 + (ay - by)**2)**0.5
         return d
 
     def validate(self):
         """Perform safety checks on self."""
-        if (not (0 < self.horiz <= MAX_HORIZ)
-                or not (0 < self.vert <= MAX_VERT)):
+        if not (0 < self.x <= MAX_X) or not (0 < self.y <= MAX_Y):
             raise AttributeError("{} is outside the map bounds".format(self))
 
 
